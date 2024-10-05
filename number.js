082f3e0277af7dc583833fb3d9e3a000623354c6
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2024-10-05 16:40:34 +0300

    Add files via upload

---
+++ number.js
@@ -0,0 +1,54 @@
+//1
+let far;
+const cels = 5;
+far = cels * 1.8 + 32;
+console.log(far);
+
+//2
+const day = 30;
+const hour = day * 24;
+const minute = hour * 60;
+console.log(hour);
+console.log(minute);
+
+//3
+let life = 100;
+let energy = 100;
+life -= 20;
+energy -= 50;
+console.log(life);
+console.log(energy);
+
+//4
+let sum = 1567;
+let sale = sum / 100 * 10;
+sum -= sale;
+console.log(sum);
+
+//5
+let float = 354.84576543456763;
+float = Math.floor(float);
+console.log(float);
+
+//6
+let floatTwo = "847.657483948579";
+floatTwo = parseFloat(floatTwo);
+console.log(floatTwo);
+
+//7
+let int = "394";
+int = parseInt(int);
+console.log(int);
+
+//8
+let sqaur = "394";
+sqaur = Math.sqrt(sqaur);
+console.log(sqaur);
+
+//9
+let intTwo;
+let strin;
+intTwo = parseInt("95");
+console.log(intTwo);
+strin = intTwo.toString();
+console.log(strin);
